7a0d1e4de46f19aaa1df2b69a4e24515a81d4687
Marcel0tm <marcelosilvatmls@gmail.com>
2023-05-25 20:23:12 -0300

    Resolvido problemas de vinculaçãtytuttyttutyo.

--- app/src/main/java/com/example/projertoeita2/MainActivity.kt
+++ app/src/main/java/com/example/projertoeita2/MainActivity.kt
@@ -15,6 +15,13 @@ class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
 
+
+
+    teste
+
+
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
