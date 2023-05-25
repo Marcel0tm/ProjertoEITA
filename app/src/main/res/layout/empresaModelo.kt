1bbf00e1518d8789426dfa45164f7851072866f5
Talisson Marcelo <marcelosilvatmls@gmail.com>
2023-05-24 22:44:07 -0300

    Configurando tela de cadastro de vagas

---
+++ app/src/main/res/layout/empresaModelo.kt
@@ -0,0 +1,8 @@
+package layout
+
+data class empresaModelo(
+    var empId: String? = null,
+    var empNome: String? = null,
+    var empCargo: String? = null,
+    var emSalario: String? = null
+)
